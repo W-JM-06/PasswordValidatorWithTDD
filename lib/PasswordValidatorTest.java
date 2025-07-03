ffde0f8704885419303ea57647320c9f8e65c62f
W-JM-06 <wisawa.ju@ku.th>
2025-07-03 20:09:12 +0700

    Add files via upload

--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -3,18 +3,57 @@ package lib;
 public class PasswordValidatorTest{
     
     public static void run() {
-        System.out.println("--- Running Password Validator Tests ---");
-        
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
-        PasswordStrength result1 = PasswordValidator.validate("123");
+        String pw = "123"  ;
+        PasswordStrength result1 = PasswordValidator.validate(pw);
         if (result1 == PasswordStrength.INVALID) {
-            System.out.println("Test Case 1 Passed: Short password is INVALID.");
+            System.out.println("Test Case 1 Passed:"+ pw+ " :Short password is INVALID.");
         } else {
             System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
+        } 
+        // Test Case 2: รหัสผ่านตัวเลขซ้ำกันควรจะ WEAK
+
+        
+        PasswordStrength result2 = PasswordValidator.validate(pw);
+        pw = "11111111";
+        if(result2 == PasswordStrength.WEAK){
+            System.out.println("Test Case 2  Passed:"+ pw+ " :Short password is WEAK.");
+        }else {
+            System.out.println("Test Case 2 FAILED:"+ pw+ " :Expected WEAK but got "+ result2);
         }
 
-        // --- เขียน Test Case อื่นๆ ต่อ ---
+        //Test Case 3: รหัสผ่านเป็นตัวอักษรengตัวเล็ก ตัวใหญ่ ควรขะ MEDIUM
+        PasswordStrength result3 = PasswordValidator.validate(pw);
+        pw = "Maxfasfgt";
+        if(result3 == PasswordStrength.MEDIUM){
+            System.out.println("Test Case 3  Passed:"+ pw+ " :Short password is MEDIUM.");
+            }else {
+            System.out.println("Test Case 3 FAILED:"+ pw+ " :Expected MEDIUM but got " + result3);
+            }
+            PasswordStrength result4 = PasswordValidator.validate(pw);
+        pw = "Max20061";
+        if(result4 == PasswordStrength.STRONG){
+            System.out.println("Test Case 4  Passed:"+ pw+ " :Short password is STRONG.");
+            }else {
+            System.out.println("Test Case 4 FAILED:"+ pw+ " :Expected STRONG but got " + result4);
+            }
+            PasswordStrength result5 = PasswordValidator.validate(pw);
+        pw = "maxangryy";
+        if(result5 == PasswordStrength.WEAK){
+            System.out.println("Test Case 5  Passed:"+ pw+ " :Short password is WEAK.");
+            }else {
+            System.out.println("Test Case 5 FAILED:"+ pw+ " :Expected WEAK but got " + result5);
+            }
 
-        System.out.println("--------------------------------");
+
+                
+
+            
+        }
+
+        
+
+        
     }
-}
+
+
