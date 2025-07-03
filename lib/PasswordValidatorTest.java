5f21e85223f68a829171674f2608281dcc87e3d4
W-JM-06 <wisawa.ju@ku.th>
2025-07-03 20:18:53 +0700

    Add files via upload

--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -13,32 +13,38 @@ public class PasswordValidatorTest{
         } 
         // Test Case 2: รหัสผ่านตัวเลขซ้ำกันควรจะ WEAK
 
-        
-        PasswordStrength result2 = PasswordValidator.validate(pw);
         pw = "11111111";
+        PasswordStrength result2 = PasswordValidator.validate(pw);
+        
         if(result2 == PasswordStrength.WEAK){
             System.out.println("Test Case 2  Passed:"+ pw+ " :Short password is WEAK.");
         }else {
             System.out.println("Test Case 2 FAILED:"+ pw+ " :Expected WEAK but got "+ result2);
+
         }
 
-        //Test Case 3: รหัสผ่านเป็นตัวอักษรengตัวเล็ก ตัวใหญ่ ควรขะ MEDIUM
-        PasswordStrength result3 = PasswordValidator.validate(pw);
+        //Test Case 3: รหัสผ่านเป็นตัวอักษรengตัวเล็ก ตัวใหญ่ ควรจะ MEDIUM
         pw = "Maxfasfgt";
+        PasswordStrength result3 = PasswordValidator.validate(pw);
+        
         if(result3 == PasswordStrength.MEDIUM){
             System.out.println("Test Case 3  Passed:"+ pw+ " :Short password is MEDIUM.");
             }else {
             System.out.println("Test Case 3 FAILED:"+ pw+ " :Expected MEDIUM but got " + result3);
             }
-            PasswordStrength result4 = PasswordValidator.validate(pw);
-        pw = "Max20061";
+        //Test Case 4: รหัสผ่านที่มีตัวอักษรengตัวเล็ก ตัวใหญ่ ตัวเลขด้วยควรจะ STRONG
+        pw = "Max2901";
+        PasswordStrength result4 = PasswordValidator.validate(pw);
+        
         if(result4 == PasswordStrength.STRONG){
             System.out.println("Test Case 4  Passed:"+ pw+ " :Short password is STRONG.");
             }else {
             System.out.println("Test Case 4 FAILED:"+ pw+ " :Expected STRONG but got " + result4);
             }
-            PasswordStrength result5 = PasswordValidator.validate(pw);
-        pw = "maxangryy";
+        //Test Case 5: รหัสผ่านเป็นตัวอักษรengตัวเล็กทั้งหมดควรจะ WEAK  
+        pw = "maxangryy";  
+        PasswordStrength result5 = PasswordValidator.validate(pw);
+        
         if(result5 == PasswordStrength.WEAK){
             System.out.println("Test Case 5  Passed:"+ pw+ " :Short password is WEAK.");
             }else {
